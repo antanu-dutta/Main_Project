0faec6480bad49d9b4e82097efd77514aa963189
Antanu Dutta <antanu.bittu@gmail.com>
2024-05-28 15:55:48 +0530

    new file

---
+++ src/Components/Honey Section/HoneySection Slider/HoneySlider.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./HoneySlider.css";
+import ProductItemdesign from "../../Product Item Design/ProductItemdesign";
+
+function HoneySlider() {
+  return <ProductItemdesign />;
+}
+
+export default HoneySlider;
--- src/Components/Honey Section/Honeysection.jsx
+++ src/Components/Honey Section/Honeysection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import "./Honeysection";
 import ProductItemdesign from "../Product Item Design/ProductItemdesign";
+import "./Honeysection.css";
+import HoneySlider from "./HoneySection Slider/HoneySlider";
 
 function Honeysection() {
   return (
@@ -13,7 +14,19 @@ function Honeysection() {
           <p className="text-[#000] text-sm font-normal w-[70%] leading-[22px] mb-4">
             Be-produced, unprocessed honey from a farm, pure and natural
           </p>
-          <div className=" mt-[80px]">
+          <div className="  flex flex-col gap-10">
+            <div className="navigation-btn flex gap-5">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0781/6711/4011/files/Variant3.svg?v=1696408269"
+                alt=""
+                className="cursor-pointer"
+              />
+              <img
+                src="https://cdn.shopify.com/s/files/1/0781/6711/4011/files/Variant3_d560f112-8f09-48f5-a26b-2f02659577d8.svg?v=1696408283"
+                alt=""
+                className="cursor-pointer"
+              />
+            </div>
             <a
               href="#"
               className=" py-[12px] px-[70px] text-[#fff] border-[1px] border-solid border-[#855245] text-[14px] inline-block font-semibold rounded-[12px] bg-[#855245] leading-[18px] uppercase"
@@ -22,8 +35,8 @@ function Honeysection() {
             </a>
           </div>
         </div>
-        <div className="w-[65%] flex">
-          <ProductItemdesign />
+        <div className="product-container w-[65%] flex flex-nowrap overflow-x-auto">
+          <HoneySlider />
         </div>
       </div>
     </div>
